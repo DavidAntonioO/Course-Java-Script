c6c67d4c66c28fa699455ec6a120bb838e34cbb7
DavidAntonioO <david-antonio.olmos@atos.net>
2022-05-10 15:25:02 -0700

    Constantes

---
+++ constantes.js
@@ -0,0 +1,14 @@
+"use strict"
+
+//Constantes
+//Contenedor de datos al igual que una variable
+//Ese valor no va a poder ser modificable
+//Es como una variable, pero su contenido nunca va a cambiar
+
+var web = 'https://www.apple.com/mx/shop/buy-iphone/iphone-11';
+const ip = '192.88.0.12';
+
+
+web= 'https://www.youtube.com/watch?v=SaVjKvCapuI';
+
+console.log(web, ip);
